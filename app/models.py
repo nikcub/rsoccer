bc7094579485980a5ea857c46c73b6a66c2b6b8e
Nik Cubrilovic <nikcub@gmail.com>
2012-08-10 06:31:24 +1000

    initial player models

--- app/models.py
+++ app/models.py
@@ -4,3 +4,25 @@ class Greeting(db.Model):
   author = db.StringProperty()
   content = db.StringProperty(multiline=True)
   date = db.DateTimeProperty(auto_now_add=True)
+
+class Match(db.Model):
+	league = db.IntegerProperty()
+	roundno = db.IntegerProperty()
+	home_team = db.ReferenceProperty(Team)
+	away_team = db.ReferenceProperty(Team)
+
+class Team(db.Model):
+	league = db.IntegerProperty()
+	name_short = db.StringProperty()
+	name_long = db.StringProperty()
+	ground_name = db.StringProperty()
+
+class Player(db.Model):
+	club = db.ReferenceProperty(Team)
+	first_name = db.StringProperty()
+	last_name = db.StringProperty()
+	position = db.StringProperty()
+	country = db.StringProperty()
+	age = db.IntegerProperty()
+
+
