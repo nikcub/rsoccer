11da17a9dd26b0211063406aa5e7be595a02f553
9jack9 <dean.edwards@gmail.com>
2013-02-04 00:43:42 +0000

    ===

--- soccerbot/link-lib.php
+++ soccerbot/link-lib.php
@@ -50,7 +50,7 @@ function spam_bot($subreddit) { // watches the spam queue
   foreach ($list as $entry) {
     if ($entry->kind == 't3') {
       $entry = $entry->data;
-      if ($entry->banned_by == TRUE) { // auto-removed
+      if ($entry->banned_by === TRUE) { // auto-removed
         if ($entry->approved_by) { // re-approve
           reddit_approve($subreddit, $entry->name);
         } else if ($entry->is_self && preg_match($match_thread, $entry->title)) {
